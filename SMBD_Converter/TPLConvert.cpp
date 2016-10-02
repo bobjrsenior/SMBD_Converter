17716f3844fabf305140dfe30ab0142b49b4d5ac
bobjrsenior <rcarpe31@uncc.edu>
2016-10-02 13:09:58 -0400

    Started work on gma coversion

--- SMBD_Converter/TPLConvert.cpp
+++ SMBD_Converter/TPLConvert.cpp
@@ -11,7 +11,28 @@ typedef struct {
 	uint16_t unknown;
 }Texture;
 
+typedef struct {
+	uint32_t modelOffsetFromBase;
+	uint32_t nameOffsetFromStartOfNames;
+	uint32_t unknown1;
+	float unknown2;
+	float unknown3;
+	float unknown4;
+	float unknown5;
+	uint32_t numTextures;
+	uint32_t numTexturesSection1;
+	uint32_t numTexturesSection2;
+	uint8_t numHeaderBlobs; //Unknown
+	uint8_t checkByte; //0
+	uint32_t endOfHeaderOffset; //Unknown
+	//Checks
+	std::string modelName;
+
+}Model;
+
 void parseTPL(char* filename);
+
+void parseGMA(char* filename);
 
 int main(int argc, char*argv[]) {
 	if (argc == 1) {
@@ -23,6 +44,9 @@ int main(int argc, char*argv[]) {
 
 	if (fileType == "tpl") {
 		parseTPL(argv[1]);
+	}
+	else if (fileType == "gma") {
+		parseGMA(argv[1]);
 	}
 
 	return 0;
@@ -180,6 +204,257 @@ void parseTPL(char* filename) {
 	}
 	std::cout << ftell(output) << std::endl;
 	std::cout << "WROTE TEXTURES" << std::endl;
+	fclose(input);
+	fclose(output);
+}
+
+void parseGMA(char* filename) {
+	std::string inputFile(filename);
+
+	std::string outputFile = inputFile + ".gc";
+	int numTextures = 0;
+	uint32_t fileSize = 0;
+
+	Model models[256];
+
+	FILE* input = fopen(filename, "rb");
+
+	if (input == NULL) {
+		std::cout << "FAILED TO OPEN" << std::endl;
+		return;
+	}
+
+	fseek(input, 0, SEEK_END);
+	fileSize = ftell(input);
+	fseek(input, 0, SEEK_SET);
+
+	FILE* output = fopen(outputFile.c_str(), "wb");
+
+	uint32_t numModels = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+	putc((numModels >> 24) & 0xFF, output);
+	putc((numModels >> 16) & 0xFF, output);
+	putc((numModels >> 8) & 0xFF, output);
+	putc(numModels & 0xFF, output);
+	
+
+	uint32_t modelDataBaseOffset = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+	putc((numModels >> 24) & 0xFF, output);
+	putc((numModels >> 16) & 0xFF, output);
+	putc((numModels >> 8) & 0xFF, output);
+	putc(numModels & 0xFF, output);
+
+	for (int i = 0; i < (int) numModels; ++i) {
+		uint32_t modelOffsetFromBase = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		models[i].modelOffsetFromBase = modelOffsetFromBase;
+
+		uint32_t nameOffsetFromStartOfNames = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		models[i].nameOffsetFromStartOfNames = nameOffsetFromStartOfNames;
+	}
+
+	fseek(output, 8 * numModels, SEEK_CUR);
+	uint32_t asciiOffset = ftell(input);
+
+	for (int i = 0; i < (int) numModels; ++i) {
+		char modelName[512];
+		int index = 0;
+		char c = getc(input);
+		while(c != 0 && index < 510) {
+			modelName[index++] = c;
+			putc(c, output);
+			c = getc(input);
+		}
+		putc(0, output);
+		modelName[index] = 0;
+		std::string modelNameStr(modelName);
+	}
+
+
+	// 0s for padding/alignment
+	while (ftell(input) < modelDataBaseOffset) {
+		putc(getc(input), output);
+	}
+
+	// GCMF
+	putc('G', output);
+	putc('C', output);
+	putc('N', output);
+	putc('F', output);
+
+	// It it FMCG on xbox
+	fseek(input, 4, SEEK_CUR);
+
+	for (int i = 0; i < numModels; ++i) {
+		/// Model Header
+
+		uint32_t unknown1 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((unknown1 >> 24) & 0xFF, output);
+		putc((unknown1 >> 16) & 0xFF, output);
+		putc((unknown1 >> 8) & 0xFF, output);
+		putc(unknown1 & 0xFF, output);
+
+		uint32_t unknown2 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((unknown1 >> 24) & 0xFF, output);
+		putc((unknown1 >> 16) & 0xFF, output);
+		putc((unknown1 >> 8) & 0xFF, output);
+		putc(unknown1 & 0xFF, output);
+
+		uint32_t unknown3 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((unknown1 >> 24) & 0xFF, output);
+		putc((unknown1 >> 16) & 0xFF, output);
+		putc((unknown1 >> 8) & 0xFF, output);
+		putc(unknown1 & 0xFF, output);
+
+		uint32_t unknown4 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((unknown1 >> 24) & 0xFF, output);
+		putc((unknown1 >> 16) & 0xFF, output);
+		putc((unknown1 >> 8) & 0xFF, output);
+		putc(unknown1 & 0xFF, output);
+
+		uint32_t unknown5 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((unknown1 >> 24) & 0xFF, output);
+		putc((unknown1 >> 16) & 0xFF, output);
+		putc((unknown1 >> 8) & 0xFF, output);
+		putc(unknown1 & 0xFF, output);
+
+		uint16_t numTextures = getc(input) + (getc(input) << 8);
+		putc((unknown1 >> 8) & 0xFF, output);
+		putc(unknown1 & 0xFF, output);
+		
+		uint16_t numTexturesSection1 = getc(input) + (getc(input) << 8);
+		putc((numTexturesSection1 >> 8) & 0xFF, output);
+		putc(numTexturesSection1 & 0xFF, output);
+
+		uint16_t numTexturesSection2 = getc(input) + (getc(input) << 8);
+		putc((numTexturesSection2 >> 8) & 0xFF, output);
+		putc(numTexturesSection2 & 0xFF, output);
+
+		// Number of header blobs/Unknown
+		putc(getc(input), output);
+
+		// 0 (For alignment probably)
+		putc(getc(input), output);
+
+		// Unknown
+		uint32_t endOfHeaderOffset = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((endOfHeaderOffset >> 24) & 0xFF, output);
+		putc((endOfHeaderOffset >> 16) & 0xFF, output);
+		putc((endOfHeaderOffset >> 8) & 0xFF, output);
+		putc(endOfHeaderOffset & 0xFF, output);
+
+		uint32_t endOfHeaderOffset = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((endOfHeaderOffset >> 24) & 0xFF, output);
+		putc((endOfHeaderOffset >> 16) & 0xFF, output);
+		putc((endOfHeaderOffset >> 8) & 0xFF, output);
+		putc(endOfHeaderOffset & 0xFF, output);
+
+		uint32_t zero1 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((zero1 >> 24) & 0xFF, output);
+		putc((zero1 >> 16) & 0xFF, output);
+		putc((zero1 >> 8) & 0xFF, output);
+		putc(zero1 & 0xFF, output);
+
+		uint32_t negativeOne = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((negativeOne >> 24) & 0xFF, output);
+		putc((negativeOne >> 16) & 0xFF, output);
+		putc((negativeOne >> 8) & 0xFF, output);
+		putc(negativeOne & 0xFF, output);
+
+
+		// Negative one in XGMA?
+		uint32_t zero2 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((zero2 >> 24) & 0xFF, output);
+		putc((zero2 >> 16) & 0xFF, output);
+		putc((zero2 >> 8) & 0xFF, output);
+		putc(zero2 & 0xFF, output);
+
+		uint32_t zero3 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((zero3 >> 24) & 0xFF, output);
+		putc((zero3 >> 16) & 0xFF, output);
+		putc((zero3 >> 8) & 0xFF, output);
+		putc(zero3 & 0xFF, output);
+
+		uint32_t zero4 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((zero4 >> 24) & 0xFF, output);
+		putc((zero4 >> 16) & 0xFF, output);
+		putc((zero4 >> 8) & 0xFF, output);
+		putc(zero4 & 0xFF, output);
+
+		uint32_t zero5 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((zero5 >> 24) & 0xFF, output);
+		putc((zero5 >> 16) & 0xFF, output);
+		putc((zero5 >> 8) & 0xFF, output);
+		putc(zero5 & 0xFF, output);
+
+		uint32_t zero6 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+		putc((zero6 >> 24) & 0xFF, output);
+		putc((zero6 >> 16) & 0xFF, output);
+		putc((zero6 >> 8) & 0xFF, output);
+		putc(zero6 & 0xFF, output);
+
+		/// Textures
+		for (int j = 0; j < numTextures; ++j) {
+
+			// Unknown/last byte = clamping
+			uint32_t clamping = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+			putc((clamping >> 24) & 0xFF, output);
+			putc((clamping >> 16) & 0xFF, output);
+			putc((clamping >> 8) & 0xFF, output);
+			putc(clamping & 0xFF, output);
+
+
+			uint16_t tplTextureNumber = getc(input) + (getc(input) << 8);
+			putc((tplTextureNumber >> 8) & 0xFF, output);
+			putc(tplTextureNumber & 0xFF, output);
+
+			// Probably padding/Unknown
+			uint16_t unknown1 = getc(input) + (getc(input) << 8);
+			putc((unknown1 >> 8) & 0xFF, output);
+			putc(unknown1 & 0xFF, output);
+
+			uint32_t zero1 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+			putc((zero1 >> 24) & 0xFF, output);
+			putc((zero1 >> 16) & 0xFF, output);
+			putc((zero1 >> 8) & 0xFF, output);
+			putc(zero1 & 0xFF, output);
+
+			uint32_t unknown2 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+			putc((unknown2 >> 24) & 0xFF, output);
+			putc((unknown2 >> 16) & 0xFF, output);
+			putc((unknown2 >> 8) & 0xFF, output);
+			putc(unknown2 & 0xFF, output);
+
+
+			// Probably padding/Unknown
+			uint16_t unknown3 = getc(input) + (getc(input) << 8);
+			putc((unknown3 >> 8) & 0xFF, output);
+			putc(unknown3 & 0xFF, output);
+
+			uint16_t textureIndex = getc(input) + (getc(input) << 8);
+			putc((textureIndex >> 8) & 0xFF, output);
+			putc(textureIndex & 0xFF, output);
+
+			uint32_t zero2 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+			putc((zero2 >> 24) & 0xFF, output);
+			putc((zero2 >> 16) & 0xFF, output);
+			putc((zero2 >> 8) & 0xFF, output);
+			putc(zero2 & 0xFF, output);
+
+			uint32_t zero3 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+			putc((zero3 >> 24) & 0xFF, output);
+			putc((zero3 >> 16) & 0xFF, output);
+			putc((zero3 >> 8) & 0xFF, output);
+			putc(zero3 & 0xFF, output);
+
+			uint32_t zero4 = getc(input) + (getc(input) << 8) + (getc(input) << 16) + (getc(input) << 24);
+			putc((zero4 >> 24) & 0xFF, output);
+			putc((zero4 >> 16) & 0xFF, output);
+			putc((zero4 >> 8) & 0xFF, output);
+			putc(zero4 & 0xFF, output);
+		}
+
+	}
+
+
 	fclose(input);
 	fclose(output);
 }
